a7242d730933af015531c90f49fe92518f883c50
Raphel Fluckiger <raphael.fluckiger@gmail.com>
2016-06-27 20:56:09 +0200

    Update facto.py

--- facto.py
+++ facto.py
@@ -1,6 +1,9 @@
-nb = int(input("what factorial do you want?"))
-prod = 1
-for i in range(nb):
-    prod = prod * (i+1)
-    if i == nb:
+while True:
+    nb = int(input("what factorial do you want?"))
+    prod = 1
+    for i in range(0, nb):
+        prod = prod * (i+1)
         print(prod)
+    reponse = input("recommencer?")
+    if reponse == 'n' or reponse == 'N':
+        break
