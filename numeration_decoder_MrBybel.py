c224ce2b0bf4aefe5f87da958057e51a153b9c6f
Raphel Fluckiger <raphael.fluckiger@gmail.com>
2016-09-11 11:50:45 +0200

    Uploaded new version of the file.
    still no base over 10.

--- numeration_decoder_MrBybel.py
+++ numeration_decoder_MrBybel.py
@@ -1,36 +1,7 @@
 #MrBybel's python numeration decoder
 #06.09.2016
-space_it_out = print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-binary_text = print("01000010 01100001 01110011 01100101  01010100 01101111  01000010 01100001 01110011 01100101\n\n\n\n")
-base_to_base_text = print('''  ____                   _          ____                 
- |  _ \                 | |        |  _ \                
- | |_) | __ _ ___  ___  | |_ ___   | |_) | __ _ ___  ___ 
- |  _ < / _` / __|/ _ \ | __/ _ \  |  _ < / _` / __|/ _ \
- | |_) | (_| \__ \  __/ | || (_) | | |_) | (_| \__ \  __/
- |____/ \__,_|___/\___|  \__\___/  |____/ \__,_|___/\___|
-                                                         
-														 
-														 
-														 
-														 ''')
-author_text = print('''  __  __        ____        _          _                                        _   _                   _                    _           
- |  \/  |      |  _ \      | |        | |                                      | | (_)                 | |                  | |          
- | \  / |_ __  | |_) |_   _| |__   ___| |  _ __  _   _ _ __ ___   ___ _ __ __ _| |_ _  ___  _ __     __| | ___  ___ ___   __| | ___ _ __ 
- | |\/| | '__| |  _ <| | | | '_ \ / _ \ | | '_ \| | | | '_ ` _ \ / _ \ '__/ _` | __| |/ _ \| '_ \   / _` |/ _ \/ __/ _ \ / _` |/ _ \ '__|
- | |  | | |    | |_) | |_| | |_) |  __/ | | | | | |_| | | | | | |  __/ | | (_| | |_| | (_) | | | | | (_| |  __/ (_| (_) | (_| |  __/ |   
- |_|  |_|_|    |____/ \__, |_.__/ \___|_| |_| |_|\__,_|_| |_| |_|\___|_|  \__,_|\__|_|\___/|_| |_|  \__,_|\___|\___\___/ \__,_|\___|_|   
-                       __/ |                                                                                                             
-                      |___/                                                                                                              
-					  
-					  
-					  
-					  ''')
-welcome_text = input("Welcome to MrBybel\'s numeration decoder! \n\n\n Press ENTER to continue:   ")
 
-starting_number = input("Please enter the starting number:  ")
-starting_base = int(input("Please enter the starting base:  "))
-ending_base = int(input("Please enter the end base:  "))
-
+reponse = 'o'
 
 def todecimal(starting_number, starting_base):
 	#transform to a string to index it
@@ -41,7 +12,7 @@ def todecimal(starting_number, starting_base):
 	
 	for i in range(0, len(starting_number_str)):
 		starting_result += int(starting_number_str[i]) * pow(starting_base, i)
-	print(starting_result)
+	print("      " + str(starting_result))
 	print("          /          ")
 	print("          \          ")
 	print("          /          ")
@@ -49,6 +20,13 @@ def todecimal(starting_number, starting_base):
 	print("          /          ")
 	print("          \          ")
 	print("          /          ")
+	print("          \          ")
+	print("          /          ")
+	print("          \          ")
+	print("          /          ")
+	print("          \          ")
+	print("          /          ")
+
 	return starting_result
 	
 	
@@ -64,7 +42,48 @@ def fromdecimal(starting_result, ending_base):
 		
 	ending_result = ending_result[::-1]
 	print("      " + ending_result)
+	
 	return ending_result
 	
 	
-fromdecimal(todecimal(starting_number, starting_base), ending_base)
+	
+	
+while True:
+	if reponse == 'o' or 'O' or 'y' or 'Y':
+		space_it_out = print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+		binary_text = print("							01000010 01100001 01110011 01100101  01010100 01101111  01000010 01100001 01110011 01100101\n\n\n\n")
+		print('''        BBBBBBBBBBBBBBBBB                                                                       tttt                                BBBBBBBBBBBBBBBBB                                                         
+	B::::::::::::::::B                                                                   ttt:::t                                B::::::::::::::::B                                                        
+	B::::::BBBBBB:::::B                                                                  t:::::t                                B::::::BBBBBB:::::B                                                       
+	BB:::::B     B:::::B                                                                 t:::::t                                BB:::::B     B:::::B                                                      
+	  B::::B     B:::::B  aaaaaaaaaaaaa      ssssssssss       eeeeeeeeeeee         ttttttt:::::ttttttt       ooooooooooo          B::::B     B:::::B  aaaaaaaaaaaaa      ssssssssss       eeeeeeeeeeee    
+	  B::::B     B:::::B  a::::::::::::a   ss::::::::::s    ee::::::::::::ee       t:::::::::::::::::t     oo:::::::::::oo        B::::B     B:::::B  a::::::::::::a   ss::::::::::s    ee::::::::::::ee  
+	  B::::BBBBBB:::::B   aaaaaaaaa:::::ass:::::::::::::s  e::::::eeeee:::::ee     t:::::::::::::::::t    o:::::::::::::::o       B::::BBBBBB:::::B   aaaaaaaaa:::::ass:::::::::::::s  e::::::eeeee:::::ee
+	  B:::::::::::::BB             a::::as::::::ssss:::::se::::::e     e:::::e     tttttt:::::::tttttt    o:::::ooooo:::::o       B:::::::::::::BB             a::::as::::::ssss:::::se::::::e     e:::::e
+	  B::::BBBBBB:::::B     aaaaaaa:::::a s:::::s  ssssss e:::::::eeeee::::::e           t:::::t          o::::o     o::::o       B::::BBBBBB:::::B     aaaaaaa:::::a s:::::s  ssssss e:::::::eeeee::::::e
+	  B::::B     B:::::B  aa::::::::::::a   s::::::s      e:::::::::::::::::e            t:::::t          o::::o     o::::o       B::::B     B:::::B  aa::::::::::::a   s::::::s      e:::::::::::::::::e 
+	  B::::B     B:::::B a::::aaaa::::::a      s::::::s   e::::::eeeeeeeeeee             t:::::t          o::::o     o::::o       B::::B     B:::::B a::::aaaa::::::a      s::::::s   e::::::eeeeeeeeeee  
+	  B::::B     B:::::Ba::::a    a:::::assssss   s:::::s e:::::::e                      t:::::t    tttttto::::o     o::::o       B::::B     B:::::Ba::::a    a:::::assssss   s:::::s e:::::::e           
+	BB:::::BBBBBB::::::Ba::::a    a:::::as:::::ssss::::::se::::::::e                     t::::::tttt:::::to:::::ooooo:::::o     BB:::::BBBBBB::::::Ba::::a    a:::::as:::::ssss::::::se::::::::e          
+	B:::::::::::::::::B a:::::aaaa::::::as::::::::::::::s  e::::::::eeeeeeee             tt::::::::::::::to:::::::::::::::o     B:::::::::::::::::B a:::::aaaa::::::as::::::::::::::s  e::::::::eeeeeeee  
+	B::::::::::::::::B   a::::::::::aa:::as:::::::::::ss    ee:::::::::::::e               tt:::::::::::tt oo:::::::::::oo      B::::::::::::::::B   a::::::::::aa:::as:::::::::::ss    ee:::::::::::::e  
+	BBBBBBBBBBBBBBBBB     aaaaaaaaaa  aaaa sssssssssss        eeeeeeeeeeeeee                 ttttttttttt     ooooooooooo        BBBBBBBBBBBBBBBBB     aaaaaaaaaa  aaaa sssssssssss        eeeeeeeeeeeeee  ''')
+		print("\n\n\n")
+		print('''         ______           ______        _           _ 
+	|  ___ \         (____  \      | |         | |
+	| | _ | | ____    ____)  )_   _| | _   ____| |
+	| || || |/ ___)  |  __  (| | | | || \ / _  ) |
+	| || || | |      | |__)  ) |_| | |_) | (/ /| |
+	|_||_||_|_|      |______/ \__  |____/ \____)_|
+		                 (____/               \n''')
+		welcome_text = input("				Welcome to MrBybel\'s numeration decoder! \n\n\n				Press ENTER to continue:   ")
+
+		starting_number = input("				Please enter the starting number:  ")
+		starting_base = int(input("				Please enter the starting base:  "))
+		ending_base = int(input("				Please enter the end base:  "))
+
+		fromdecimal(todecimal(starting_number, starting_base), ending_base)
+
+		reponse = input("				Retry?:  ")
+	else:
+		exit()
